3cf89722635adb70361d3e65c1268aeb85dcfbcd
Luisa Barrios <luisabarrios01@gmail.com>
2024-08-02 16:10:46 -0300

    production url agregada

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
-// config.js
 const environment = import.meta.env.VITE_ENVIRONMENT;
+const productionUrl= import.meta.env.VITE_PRODUCTION_URL;
 console.log(environment)
 
-const ApiUrl = environment === "prod" ? "" : environment === "dev" ? "http://localhost:3000" : null;
+const ApiUrl = environment === "prod" ? productionUrl : environment === "dev" ? "http://localhost:3000" : null;
 
 export { ApiUrl };
